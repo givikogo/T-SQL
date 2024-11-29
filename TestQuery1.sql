e05193fb6c77c0562a572dad6733e3842cf838f7
Givi Koguashvili <givi.kogo@gmail.com>
2024-11-29 15:59:29 +0400

    Update TestQuery1.sql

--- TestQuery1.sql
+++ TestQuery1.sql
@@ -1,13 +1,16 @@
-CREATE DATABASE GiviTest;
+CREATE DATABASE GiviTest
+GO
 
 CREATE TABLE givi
 (
 ID INT NOT NULL, Fname VARCHAR(25)
 )
+GO
 INSERT INTO givi
 	(ID, Fname)
 VALUES 
 	(1,'kogo'),
 	(2,'Logo'),
-	(3,'Logo3');
+	(3,'Logo3')
+GO
 	
