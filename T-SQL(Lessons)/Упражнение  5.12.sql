52d42c344fc51758efbf81cc9dd244cbfe6aef3c
Givi Koguashvili <givi.kogo@gmail.com>
2024-12-03 15:03:19 +0400

    Упражнение 5.12

---
+++ T-SQL(Lessons)/Упражнение  5.12.sql
@@ -0,0 +1,5 @@
+INSERT INTO
+    orders
+(orderid, customerid, orderdate, shippeddate, freight, shipname, shipaddress, quantity)
+VALUES
+   (10, 'ord0l', getdate(), getdate(), 100.0, 'Windstar', 'Ocean', 1);
